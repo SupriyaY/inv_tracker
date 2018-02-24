9a8da2a836f4f305f7b1292cc3e848f748a36e06
Supriya <supriya.yerramilli@gmail.com>
2018-02-24 10:51:51 -0500

    adding 100% to height and width of body

--- client/src/components/StyledComponents/HeaderFooter.js
+++ client/src/components/StyledComponents/HeaderFooter.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 
 export const Header = styled.header`
-   height: 10vh;
+   height: 15vh;
     width: 100vw;
     background-color: #2B3B53;
     align-self: flex-start;
