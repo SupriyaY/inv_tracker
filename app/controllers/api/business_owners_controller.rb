3262a20943958d44a0fa25bff0c01cea1e9d0999
Supriya <supriya.yerramilli@gmail.com>
2018-02-26 17:03:58 -0500

    able to view object

--- app/controllers/api/business_owners_controller.rb
+++ app/controllers/api/business_owners_controller.rb
@@ -1,2 +1,32 @@
 class Api::BusinessOwnersController < ApplicationController
+    def index
+    @business_owners = BusinessOwner.all
+    render json: @business_owners
+    end
+    
+#     def show
+#     @user = User.find(params[:id])
+#     render json: @user 
+#     end
+
+#     def create
+#     @user = User.create!(user_params)
+#     render json: @user
+#     end
+
+#     def update
+#     @user = User.find(params[:id])
+#     @user.update!(user_params)
+#     render json: @user
+#     end
+
+#     def destroy
+#     @user = User.find(params[:id]).delete
+#     render status: :ok
+#     end
+
+#     private
+#     def user_params
+#         params.require(:user).permit(:name, :photo_url)
+#     end
 end
