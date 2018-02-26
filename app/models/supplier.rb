8bbe33439df08b03b0631e0a3d8bdf37095ac4b4
Supriya <supriya.yerramilli@gmail.com>
2018-02-26 14:37:23 -0500

    create and migrate db

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,5 +1,5 @@
 class Supplier < ApplicationRecord
-    has_many :bo_inv_suppliers dependent :destory
+    # has_many :bo_inv_suppliers dependent :destory
     has_many :inventories, through: :bo_inv_suppliers
     has_many :business_owners, through: :bo_inv_suppliers
 end
