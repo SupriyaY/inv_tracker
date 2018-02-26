75e207766b1324516885357a548632fe5a874bfe
Supriya <supriya.yerramilli@gmail.com>
2018-02-26 13:21:32 -0500

    updated user models

--- app/models/business_owner.rb
+++ app/models/business_owner.rb
@@ -1,3 +1,4 @@
 class BusinessOwner < ApplicationRecord
     has_many :inventories
+    has_many :bo_inv_supplier
 end
--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,3 +1,4 @@
 class Inventory < ApplicationRecord
     has_many :suppliers
+    hs_many :bo_inv_supplier
 end
