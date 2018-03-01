6d02d67058bf73429724c60b6789949dff5a426c
Supriya <supriya.yerramilli@gmail.com>
2018-03-01 12:10:25 -0500

    updated the body size

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -30,6 +30,7 @@ return (
                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
                 anim id est laborum.</p>
         <Link to="/business_owners">Business Owners</Link> 
+            <Link to="/categories">Categories</Link> 
 </WordsContainer>
 
 <ContainerHome>
