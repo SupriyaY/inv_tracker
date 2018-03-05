9c82f4c37757f73ac19066d109f88211836560fb
Supriya <supriya.yerramilli@gmail.com>
2018-03-05 13:10:22 -0500

    created validates for business owners models as a part of requirements

--- app/models/business_owner.rb
+++ app/models/business_owner.rb
@@ -2,4 +2,12 @@ class BusinessOwner < ApplicationRecord
     has_many :bo_inv_suppliers, dependent: :destroy
     has_many :inventories, through: :bo_inv_suppliers
     has_many :suppliers, through: :bo_inv_suppliers
+
+validates :name, presence: true
+validates :business_name, presence: true
+validates :business_description, presence: true
+validates :business_address, presence: true
+validates :business_email, presence: true
+validates :business_phone, presence: true
+
 end
